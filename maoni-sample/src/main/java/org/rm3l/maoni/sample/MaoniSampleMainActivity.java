08124722bc9c507a122e6cd375fd1e6dab043ab7
Armel S <armel@rm3l.org>
2016-05-11 21:08:18 +0200

    Add 'About Libraries' library

--- maoni-sample/src/main/java/org/rm3l/maoni/sample/MaoniSampleMainActivity.java
+++ maoni-sample/src/main/java/org/rm3l/maoni/sample/MaoniSampleMainActivity.java
@@ -6,7 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.mikepenz.aboutlibraries.Libs;
+import com.mikepenz.aboutlibraries.LibsBuilder;
 
 public class MaoniSampleMainActivity extends AppCompatActivity {
 
@@ -42,4 +47,31 @@ public class MaoniSampleMainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_maoni_sample, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.action_about:
+                new LibsBuilder()
+                        //Pass the fields of your application to the lib so it can find all external lib information
+                        .withFields(R.string.class.getFields())
+                        .withActivityTitle(getString(R.string.action_about))
+                        //provide a style (optional) (LIGHT, DARK, LIGHT_DARK_TOOLBAR)
+                        .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
+                        //start the activity
+                        .start(this);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
