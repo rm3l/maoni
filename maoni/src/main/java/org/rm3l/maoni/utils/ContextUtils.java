7602509f398655e4ac5a695d9bf4bac49baee0bd
Dennis Deng <dennisdeng2002@yahoo.com>
2021-03-21 19:01:29 -0400

    Add ability to capture dialog window using Maoni.start(dialog)

--- maoni/src/main/java/org/rm3l/maoni/utils/ContextUtils.java
+++ maoni/src/main/java/org/rm3l/maoni/utils/ContextUtils.java
@@ -21,7 +21,9 @@
  */
 package org.rm3l.maoni.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -53,8 +55,17 @@ public final class ContextUtils {
                     .getField(fieldName)
                     .get(null);
         } catch (final Exception e) {
-            //No worries
-            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Nullable
+    public static Activity scanForActivity(@NonNull final Context context) {
+        if (context instanceof Activity) {
+            return (Activity) context;
+        } else if (context instanceof ContextWrapper) {
+            return scanForActivity(((ContextWrapper) context).getBaseContext());
+        } else {
             return null;
         }
     }
