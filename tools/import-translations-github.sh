bdd388ac9a303b05e9882707996a90f8b0a4eca9
Armel Soro <armel@rm3l.org>
2022-04-23 00:11:10 +0200

    Update Copyright dates

--- tools/import-translations-github.sh
+++ tools/import-translations-github.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2016 Armel Soro
+# Copyright (c) 2016-2022 Armel Soro
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
